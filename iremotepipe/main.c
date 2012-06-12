ae0ca22887130e0f4849d36b498943c040a11160
Steven Wittens <steven@acko.net>
2012-06-12 00:50:13 -0700

    Initial Commit

---
+++ iremotepipe/main.c
@@ -0,0 +1,18 @@
+//
+//  main.c
+//  iremotepipe
+//
+//  Created by Steven Wittens on 12-06-12.
+//  Copyright (c) 2012 __MyCompanyName__. All rights reserved.
+//
+
+#include <stdio.h>
+
+int main (int argc, const char * argv[])
+{
+
+    // insert code here...
+    printf("Hello, World!\n");
+    return 0;
+}
+
