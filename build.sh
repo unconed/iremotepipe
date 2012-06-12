6a17a9c0a1c44c5656fc21a1d89a4a8fd98f0da7
Steven Wittens <steven@acko.net>
2012-06-12 03:12:52 -0700

    build

--- build.sh
+++ build.sh
@@ -1,5 +1,2 @@
-mkdir build
-pushd build
 OPTS="-framework Foundation -framework IOKit -framework Cocoa -std=c99 -O2 -Wall -force_cpusubtype_ALL -mmacosx-version-min=10.5 -arch i386 -arch x86_64"
-gcc $OPTS ../iremotepipe/AppleRemote.m ../iremotepipe/AppleRemoteDelegate.m ../iremotepipe/iremotepipe.m -o iremotepipe
-popd
+gcc $OPTS src/AppleRemote.m src/AppleRemoteDelegate.m src/iremotepipe.m -o iremotepipe
